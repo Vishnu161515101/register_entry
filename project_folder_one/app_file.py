e34b49dc5b3fffae114c264bfb90537e9ffc3406
Vishnu161515101 <bumannagarivishnu143@gmail.com>
2024-03-11 16:25:07 +0530

    tenth one

--- project_folder_one/app_file.py
+++ project_folder_one/app_file.py
@@ -7,11 +7,21 @@ from flask import request
 from flask import render_template
 from flask import Flask
 from flask import redirect
-
-# initialize first flask
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
+from flask_mail import Mail, Message 
+import random
 app = Flask(__name__)
-app.secret_key = 'xyzsdfg'
 
+# Flask-Mail configuration
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'jayaramireddy063@gmail.com'  # Replace with your email address
+app.config['MAIL_PASSWORD'] = 'qwgh ulfg xtnl zgaf'  # Replace with your email password
+
+
+mail = Mail(app)
 # Create a single time database connection object
 db_mysql = pymysql.connect(host='localhost', user='root', password='', db='vishnu')
 
@@ -68,6 +78,9 @@ def Register():
 				sql="insert into login(name,password,email_id)values('{}','{}','{}')".format(userName,password,email)
 				db_cursor.execute(sql)
 				db_mysql.commit()
+				msg = Message("Hello",sender="jayaramireddy063@gmail.com",recipients=["vishnuvardhan12345@gmail.com"])
+				mail.send(msg)
+				
 				message = 'You have successfully registered !'
 				# return 'hello vishnu this register page'
 			# return 'hello'
@@ -78,11 +91,36 @@ def Register():
 
 @app.route('/logout')
 def logout():
-    # session.pop('loggedin', None)
-    # session.pop('userid', None)
-    # session.pop('email', None)
+    session.pop('loggedin', None)
+    session.pop('userid', None)
+    session.pop('email', None)
     return redirect(url_for('login'))
 
+@app.route('/test')
+def test():
+	return render_template('Register1234.html')
+@app.route('/Register1234', methods=['POST'])
+def Register1234():
+    if request.method == 'POST':
+        userName = request.form['userName']
+        password = request.form['password']
+        email = request.form['email']
+
+        # Assuming you have already established a database connection and have a cursor
+        # Performing database operations...
+        
+        sql = "insert into login(name,password,email_id) values('{}','{}','{}')".format(userName, password, email)
+        db_cursor.execute(sql)
+        db_mysql.commit()
+
+        number = random.randint(1111, 9999)
+        msg = Message(subject=message, sender='jayaramireddy063@gmail.com', recipients=[email])
+        msg.body = f"{body}, {number}"  # Concatenate message body and random number
+        mail.send(msg)
+        return "Message sent!"
+
+        message = 'You have successfully registered !'
+        return message
 
 # run code in debug mode
 if __name__ == "__main__":
